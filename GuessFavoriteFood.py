1f4df5e96f355002af6ddcdf5e3b3d57925bd550
rudilazuli <100395138+rudilazuli@users.noreply.github.com>
2022-06-01 13:47:25 -0700

    Create GuessFavoriteFood.py
    A mini guessing game on favorite food.

---
+++ GuessFavoriteFood.py
@@ -0,0 +1,10 @@
+print("Hello there!")
+
+guess = input("Can you guess my favorite food?")
+
+if guess == "Tacos":
+    print("Yep! So amazing!")
+else:
+    print("Yuck! That's not it!")
+
+print("Thanks for playing!!")
